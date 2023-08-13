dd38e63c7b40493617e7d55c8ff6035d6bb61902
Rajatsingh4211 <141514027+Rajatsingh4211@users.noreply.github.com>
2023-08-13 16:30:41 +0530

    Adding two numbers by user input

---
+++ Adding_two_no_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() 
+{
+
+int a, b;
+printf("Enter the value of a\n");
+scanf("%d", &a);
+
+printf("Enter the value of a\n");
+scanf("%d", &b);
+
+printf("The value of a and b is %d", a+b); 
+return 0;
+
+}

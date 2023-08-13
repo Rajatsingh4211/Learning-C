0c9896bc4e03914658c5804250b884f60e504af3
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-08-14 00:05:19 +0530

    Simple Interest code in C

---
+++ Simple_Interest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+ int principal, rate, years;
+ int SimpleInterest; 
+
+
+
+ printf("Enter the value of principal\n");
+ scanf("%d", &principal);
+
+printf("Enter the value of rate\n");
+ scanf("%d", &rate); 
+
+printf("Enter the number of year\n");
+ scanf("%d", &years); 
+
+SimpleInterest = (principal * rate * years);
+
+printf ("The value of SI is %d", SimpleInterest);
+    return 0;
+}
